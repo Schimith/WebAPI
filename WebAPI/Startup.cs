2990f3474b0be80ef9f37e41f1f4ac34c6c0c9f0
schimith <schimithlarissa@hotmail.com>
2021-03-10 20:18:32 -0300

    teste

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -52,6 +52,8 @@ namespace WebAPI
 
             //Pedidoss por produtos
             services.AddTransient<IPedidoProdutoService, PedidoProdutoService>();
+
+            //
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
